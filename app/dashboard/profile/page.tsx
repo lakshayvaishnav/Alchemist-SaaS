b05bfd66054ce9d88e283d09288887c99d1b84f0
Lakshay Vaishnav <thelakshayvaishnav@gmail.com>
2024-11-21 23:27:30 +0530

    added bento grids in profile page

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,26 +1,66 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { BentoGrid, BentoGridItem } from "@/components/Ui/bento-grid";
+import {
+  IconClipboardCopy,
+  IconFileBroken,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
+
 export default function profile() {
   return (
-    <>
-      <div className="flex flex-1">
-        <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-          <div className="flex gap-2">
-            {[...new Array(4)].map((i) => (
-              <div
-                key={"first" + i}
-                className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-              ></div>
-            ))}
-          </div>
-          <div className="flex gap-2 flex-1">
-            {[...new Array(2)].map((i) => (
-              <div
-                key={"second" + i}
-                className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
-              ></div>
-            ))}
-          </div>
-        </div>
+    <div className="flex flex-1">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+        <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+          {items.map((item, i) => (
+            <BentoGridItem
+              key={i}
+              title={item.title}
+              description={item.description}
+              header={item.header}
+              className={item.className}
+              icon={item.icon}
+            />
+          ))}
+        </BentoGrid>
       </div>
-    </>
+    </div>
   );
 }
+
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
+);
+
+const items = [
+  {
+    title: "Fetch details using github api",
+    description:
+      "will add button here in future to add github username and then fetch it using its api",
+    header: <Skeleton />,
+    className: "md:col-span-2",
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "user details using next-auth google",
+    description: "fetch it using getserver session",
+    header: <Skeleton />,
+    className: "md:col-span-1",
+    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "let user add Project 1",
+    description: "make it hidden if not defined...",
+    header: <Skeleton />,
+    className: "md:col-span-1",
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "user project 2 ",
+    description: "hidden if not defined...",
+    header: <Skeleton />,
+    className: "md:col-span-2",
+    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  },
+];
